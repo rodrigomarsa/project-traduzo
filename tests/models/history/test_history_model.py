626206bd58d32a658a9ccd58685f37edfd2dd9f2
Rodrigo <rodrigomartins.agro@gmail.com>
2023-08-15 13:11:37 -0300

    test: add test_history_model

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,25 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    history = HistoryModel.list_as_json()
+    json_data = json.loads(history)
+    expected = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
+    assert len(json_data) == 2
+    assert (
+        json_data[0]["text_to_translate"] == expected[0]["text_to_translate"]
+    )
+    assert json_data[1]["translate_to"] == expected[1]["translate_to"]
