0ebb2dc01b1088e27064094f7087e3de07eee724
Rodrigo <rodrigomartins.agro@gmail.com>
2023-08-15 16:01:12 -0300

    feat: add history_controller

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template, request
 from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
-
-# from models.history_model import HistoryModel
+from models.history_model import HistoryModel
 
 
 translate_controller = Blueprint("translate_controller", __name__)
@@ -11,22 +10,23 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    text_to_translate = (
-        request.form["text-to-translate"] if request.method == "POST" else ""
-    )
-    translate_from = (
-        request.form["translate-from"] if request.method == "POST" else "pt"
-    )
-    translate_to = (
-        request.form["translate-to"] if request.method == "POST" else "en"
-    )
+    text_to_translate = request.form.get("text-to-translate", "")
+    translate_from = request.form.get("translate-from", "pt")
+    translate_to = request.form.get("translate-to", "en")
     translated = (
         GoogleTranslator(source=translate_from, target=translate_to).translate(
             text_to_translate
         )
-        if request.method == "POST"
+        if request.form
         else "Tradução"
     )
+    HistoryModel(
+        {
+            "text_to_translate": text_to_translate,
+            "translate_from": translate_from,
+            "translate_to": translate_to,
+        }
+    ).save() if request.method == "POST" else None
 
     return render_template(
         "index.html",
