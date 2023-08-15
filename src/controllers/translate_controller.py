2d9b6a74c7cf4bd304897f625f89a479f21a5ced
Rodrigo <rodrigomartins.agro@gmail.com>
2023-08-14 23:14:12 -0300

    feat: add methods get/post route '/'

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
-# from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
+from models.language_model import LanguageModel
+
 # from models.history_model import HistoryModel
 
 
@@ -10,7 +11,31 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    text_to_translate = (
+        request.form["text-to-translate"] if request.method == "POST" else ""
+    )
+    translate_from = (
+        request.form["translate-from"] if request.method == "POST" else "pt"
+    )
+    translate_to = (
+        request.form["translate-to"] if request.method == "POST" else "en"
+    )
+    translated = (
+        GoogleTranslator(source=translate_from, target=translate_to).translate(
+            text_to_translate
+        )
+        if request.method == "POST"
+        else "Tradução"
+    )
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated,
+    )
 
 
 # Req. 6
