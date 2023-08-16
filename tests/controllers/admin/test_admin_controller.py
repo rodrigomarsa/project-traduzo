45fb98304f9dd6366651eb44de289ad4038528b8
Rodrigo <rodrigomartins.agro@gmail.com>
2023-08-16 14:36:02 -0300

    test: add test_admin_controller

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,26 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    user = UserModel(
+        {"name": "test_admin", "level": "admin", "token": "test_token"}
+    )
+    user.save()
+    history = HistoryModel(
+        {
+            "text_to_translate": "Test translation",
+            "translate_from": "en",
+            "translate_to": "pt",
+        }
+    )
+    history.save()
+    response = app_test.delete(
+        f"/admin/history/{history.id}",
+        headers={
+            "Authorization": "test_token",
+            "User": "test_admin",
+        },
+    )
+    assert response.status_code == 204
+    assert response.json is None
